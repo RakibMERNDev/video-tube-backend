409d96202d758a6a447c57455fb4f324943b3592
abdullahalrakib727 <abdullahalrakib30@gmail.com>
2024-07-06 15:03:05 +0600

    added utility classes for API error handling and response management

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,32 @@
+
+
+//! Method 1
+const asyncHandler = (fn) => async (req, res, next) => {
+  try {
+    await fn(req, res, next);
+  } catch (error) {
+    res.status(error.code || 500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export { asyncHandler };
+
+// const asyncHandler = () =>{}
+// const asyncHandler = ( ) => { ()=> {}}
+// const asyncHandler = (fn) => ()=>{
+
+// }
+
+//! Method 2
+
+// const asyncHandler = (requestHandler) => {
+//   (req, res, next) => {
+//     Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+//       next(error)
+//     );
+//   };
+// };
+
