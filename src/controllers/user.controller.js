dea96e4018544c57729a361def50c3a97f33ca8a
abdullahalrakib727 <abdullahalrakib30@gmail.com>
2024-11-08 22:41:26 +0600

    add login functionality; implemented user login logic with email/username validation and password verification; added token generation for authenticated users

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,35 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+const generateAccessAndRefreshToken = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({
+      validateBeforeSave: false,
+    });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(500, "Something went wrong while generating tokens");
+  }
+};
+
+/** 
+   // get user details from frontend
+  // validation - not empty
+  // check if user already exists : username, email
+  // check for images, check for avatar
+  // upload image to cloudinary
+  //  create user object - create entry in db
+  // remove password and refresh token filed from response
+  // check for user creation
+  // return response
+ **/
+
 const registerUser = asyncHandler(async (req, res) => {
   const { fullName, email, username, password } = req.body;
 
@@ -30,9 +59,12 @@ const registerUser = asyncHandler(async (req, res) => {
 
   let coverImageLocalPath;
 
-  if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
     coverImageLocalPath = req.files.coverImage[0].path;
-
   }
 
   if (!avatarLocalPath) {
@@ -71,16 +103,31 @@ const registerUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(201, createdUser, "User registered successfully"));
 });
 
-export { registerUser };
+// first get email/user from request body
+// check if user exists
+// check if password is correct
+// generate access token and refresh token
+// send cookies to user
 
-/** 
-   // get user details from frontend
-  // validation - not empty
-  // check if user already exists : username, email
-  // check for images, check for avatar
-  // upload image to cloudinary
-  //  create user object - create entry in db
-  // remove password and refresh token filed from response
-  // check for user creation
-  // return response
- **/
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, username, password } = req.body;
+
+  if (!email || !username) {
+    throw new ApiError(400, "Email or username is required");
+  }
+  const user = await User.findOne({
+    $or: [{ email }, { username }],
+  });
+
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  const isPasswordValid = await user.isPassCorrect(password);
+
+  if (!isPasswordValid) {
+    throw new ApiError(401, "Invalid user credentials");
+  }
+});
+
+export { registerUser, loginUser };
