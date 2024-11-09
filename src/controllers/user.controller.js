8009cdc48441c2a5c5c18cb8100909082c2fca92
abdullahalrakib727 <abdullahalrakib30@gmail.com>
2024-11-09 22:07:47 +0600

    fix registration validation; updated user registration logic to require either email or username

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -112,7 +112,7 @@ const registerUser = asyncHandler(async (req, res) => {
 const loginUser = asyncHandler(async (req, res) => {
   const { email, username, password } = req.body;
 
-  if (!email || !username) {
+  if (!(username || email)) {
     throw new ApiError(400, "Email or username is required");
   }
   const user = await User.findOne({
