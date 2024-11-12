2c5f96440c6d7569bd3e614790c4036ce67dee37
abdullahalrakib727 <abdullahalrakib30@gmail.com>
2024-11-12 21:12:01 +0600

    refactor playlist model schema for improved readability and consistency

--- src/models/playlist.model.js
+++ src/models/playlist.model.js
@@ -1,24 +1,27 @@
 import mongoose, { Schema } from "mongoose";
 
-
-const playlistSchema = new Schema({
-    name : {
-        type: String,
-        required: true,
+const playlistSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    description : {
-        type: String,
-        required: true,
+    description: {
+      type: String,
+      required: true,
     },
-    videos : [{
+    videos: [
+      {
         type: Schema.Types.ObjectId,
         ref: "Video",
-    }],
-    owner : {
-        type: Schema.Types.ObjectId,
-        ref: "User",
+      },
+    ],
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
     },
-}, { timestamps: true });
-
+  },
+  { timestamps: true }
+);
 
 export const Playlist = mongoose.model("Playlist", playlistSchema);
